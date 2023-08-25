808e286c048bb8ac5683db173ac9b9de96d9261d
GauravJhamfp <gaurav.jha.1703@gmail.com>
2023-08-25 23:25:55 +0530

    First Commit

---
+++ src/component/Basics/menuApi.js
@@ -0,0 +1,74 @@
+const Menu = [
+    {
+        id: 1,
+        image: "images/meggi.jpg",
+        name: "maggi",
+        category: "breakfast",
+        price: "Rs.15",
+        description: "I love Maggi realy so yammy. its too easy for cook , within 2 min this is ready for eat."
+    },
+
+    {
+        id: 2,
+        image: "images/roll.jpg",
+        name: "Roll",
+        category: "breakfast",
+        price: "Rs.30",
+        description: "I love Maggi realy so yammy. its too easy for cook , within 2 min this is ready for eat."
+    },
+
+    {
+        id: 3,
+        image: "images/chowmin.jpg",
+        name: "Chowmin",
+        category: "breakfast",
+        price: "Rs.25",
+        description: "I love Maggi realy so yammy. its too easy for cook , within 2 min this is ready for eat."
+    },
+
+    {
+        id: 4,
+        image: "images/sahi paneer.jpg",
+        name: "Sahi Paneer",
+        category: "dinner",
+        price: "Rs.250",
+        description: "I love Maggi realy so yammy. its too easy for cook , within 2 min this is ready for eat."
+    },
+
+    {
+        id: 5,
+        image: "images/chicken-handi.jpg",
+        name: "Chicken Handi",
+        category: "dinner",
+        price: "Rs.275",
+        description: "I love Maggi realy so yammy. its too easy for cook , within 2 min this is ready for eat."
+    },
+
+    {
+        id: 6,
+        image: "images/snakes.jpg",
+        name: "Snakes",
+        category: "evening",
+        price: "Rs.25",
+        description: "I love Maggi realy so yammy. its too easy for cook , within 2 min this is ready for eat."
+    },
+
+    {
+        id: 7,
+        image: "images/Dal-Makhani.jpg",
+        name: "Dal Makhani",
+        category: "lunch",
+        price: "Rs.200",
+        description: "I love Maggi realy so yammy. its too easy for cook , within 2 min this is ready for eat."
+    },
+
+    {
+        id: 8,
+        image: "images/naan.jpg",
+        name: "Buttor Naan",
+        category: "dinner",
+        price: "Rs.45",
+        description: "I love Maggi realy so yammy. its too easy for cook , within 2 min this is ready for eat."
+    },
+];
+export default Menu;
